b8ddecdd5d2153f0d5f6277e77eff6057d3df8d3
AVIRAL GUPTA <91749530+aviral10x@users.noreply.github.com>
2023-10-03 23:51:37 +0530

    aa

--- src/components/Minter.tsx
+++ src/components/Minter.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react';
+import { useState } from "react";
 import { ethers } from "ethers";
-import abi from "../utils/abi.json"
-import { 
-  IHybridPaymaster, 
+import abi from "../utils/abi.json";
+import {
+  IHybridPaymaster,
   SponsorUserOperationDto,
-  PaymasterMode
-} from '@biconomy/paymaster'
-import { BiconomySmartAccount } from "@biconomy/account"
-import { toast, ToastContainer } from 'react-toastify';
-import styles from '@/styles/Home.module.css'
-import 'react-toastify/dist/ReactToastify.css';
+  PaymasterMode,
+} from "@biconomy/paymaster";
+import { BiconomySmartAccount } from "@biconomy/account";
+import { toast, ToastContainer } from "react-toastify";
+import styles from "@/styles/Home.module.css";
+import "react-toastify/dist/ReactToastify.css";
 
-const nftAddress = "0x0a7755bDfb86109D9D403005741b415765EAf1Bc"
+const nftAddress = "0x4077274f32961580e31B3Ab476231066dCE7d7bc";
 
 interface Props {
-  smartAccount: BiconomySmartAccount,
-  address: string,
-  provider: ethers.providers.Provider,
+  smartAccount: BiconomySmartAccount;
+  address: string;
+  provider: ethers.providers.Provider;
 }
 
 const Minter: React.FC<Props> = ({ smartAccount, address, provider }) => {
-  const [minted, setMinted] = useState(false)
+  const [minted, setMinted] = useState(false);
 
   const handleMint = async () => {
-    const contract = new ethers.Contract(
-      nftAddress,
-      abi.abi,
-      provider,
-    )
+    const contract = new ethers.Contract(nftAddress, abi.abi, provider);
     try {
-      toast.info('Minting your NFT...', {
+      toast.info("Minting your NFT...", {
         position: "top-right",
         autoClose: 15000,
         hideProgressBar: false,
@@ -38,16 +34,16 @@ const Minter: React.FC<Props> = ({ smartAccount, address, provider }) => {
         draggable: true,
         progress: undefined,
         theme: "dark",
-        });
+      });
       const minTx = await contract.populateTransaction.safeMint(address);
       console.log(minTx.data);
       const tx1 = {
         to: nftAddress,
         data: minTx.data,
       };
-      console.log("here before userop")
+      console.log("here before userop");
       let userOp = await smartAccount.buildUserOp([tx1]);
-      console.log({ userOp })
+      console.log({ userOp });
       const biconomyPaymaster =
         smartAccount.paymaster as IHybridPaymaster<SponsorUserOperationDto>;
       let paymasterServiceData: SponsorUserOperationDto = {
@@ -58,46 +54,58 @@ const Minter: React.FC<Props> = ({ smartAccount, address, provider }) => {
           userOp,
           paymasterServiceData
         );
-        
+
       userOp.paymasterAndData = paymasterAndDataResponse.paymasterAndData;
       const userOpResponse = await smartAccount.sendUserOp(userOp);
       console.log("userOpHash", userOpResponse);
       const { receipt } = await userOpResponse.wait(1);
       console.log("txHash", receipt.transactionHash);
-      setMinted(true)
-      toast.success(`Success! Here is your transaction:${receipt.transactionHash} `, {
-        position: "top-right",
-        autoClose: 18000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "dark",
-        });
+      setMinted(true);
+      toast.success(
+        `Success! Here is your transaction:${receipt.transactionHash} `,
+        {
+          position: "top-right",
+          autoClose: 18000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "dark",
+        }
+      );
     } catch (err: any) {
       console.error(err);
-      console.log(err)
+      console.log(err);
     }
-  }
-  return(
+  };
+  return (
     <>
-    {address && <button onClick={handleMint} className={styles.connect}>Mint NFT</button>}
-    {minted && <a href={`https://testnets.opensea.io/${address}`}> Click to view minted nfts for smart account</a>}
-    <ToastContainer
-    position="top-right"
-    autoClose={5000}
-    hideProgressBar={false}
-    newestOnTop={false}
-    closeOnClick
-    rtl={false}
-    pauseOnFocusLoss
-    draggable
-    pauseOnHover
-    theme="dark"
-    />
+      {address && (
+        <button onClick={handleMint} className={styles.connect}>
+          Mint NFT
+        </button>
+      )}
+      {minted && (
+        <a href={`https://testnets.opensea.io/${address}`}>
+          {" "}
+          Click to view minted nfts for smart account
+        </a>
+      )}
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
     </>
-  )
-}
+  );
+};
 
 export default Minter;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,81 +1,98 @@
-import Head from 'next/head'
+import Head from "next/head";
+import { ParticleAuthModule, ParticleProvider } from "@biconomy/particle-auth";
 import {
-  ParticleAuthModule,
-  ParticleProvider,
-} from "@biconomy/particle-auth";
-import styles from '@/styles/Home.module.css'
-import { useState } from 'react';
-import { IBundler, Bundler } from '@biconomy/bundler'
-import { BiconomySmartAccount, BiconomySmartAccountConfig, DEFAULT_ENTRYPOINT_ADDRESS } from "@biconomy/account"
-import { ethers  } from 'ethers'
-import { ChainId } from "@biconomy/core-types"
-import { 
-  IPaymaster, 
-  BiconomyPaymaster,  
-} from '@biconomy/paymaster'
-import Minter from '@/components/Minter';
-
-
+  Avalanche,
+  BSC,
+  BSCTestnet,
+  Ethereum,
+  EthereumGoerli,
+  KCCTestnet,
+  Moonbeam,
+  Moonriver,
+  Optimism,
+  PlatON,
+  Polygon,
+  Solana,
+} from "@particle-network/common";
+import styles from "@/styles/Home.module.css";
+import { useState } from "react";
+import { IBundler, Bundler } from "@biconomy/bundler";
+import {
+  BiconomySmartAccount,
+  BiconomySmartAccountConfig,
+  DEFAULT_ENTRYPOINT_ADDRESS,
+} from "@biconomy/account";
+import { ethers } from "ethers";
+import { ChainId } from "@biconomy/core-types";
+import { IPaymaster, BiconomyPaymaster } from "@biconomy/paymaster";
+import Minter from "@/components/Minter";
 
 export default function Home() {
-  const [address, setAddress] = useState<string>("")
+  const [address, setAddress] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
-  const [smartAccount, setSmartAccount] = useState<BiconomySmartAccount | null>(null);
-  const [provider, setProvider] = useState<ethers.providers.Provider | null>(null)
+  const [smartAccount, setSmartAccount] = useState<BiconomySmartAccount | null>(
+    null
+  );
+  const [provider, setProvider] = useState<ethers.providers.Provider | null>(
+    null
+  );
 
   const particle = new ParticleAuthModule.ParticleNetwork({
     //https://docs.particle.network/getting-started/dashboard
-    projectId: "<from particle dashboard>",
-    clientKey: "<from particle dashboard>",
-    appId: "<from particle dashboard>",
+    projectId: "9907c5b3-a301-438e-bd84-2de8c100a335",
+    clientKey: "cJEtubDg5Q2PYJadqwaClRo2dLaYtpwWFN1IOniQ",
+    appId: "7fae25d0-57a1-42c8-9569-79b045ddb48c",
     wallet: {
       displayWalletEntry: true,
       defaultWalletEntryPosition: ParticleAuthModule.WalletEntryPosition.BR,
+      supportChains: [Ethereum, EthereumGoerli],
     },
   });
 
   const bundler: IBundler = new Bundler({
     //https://dashboard.biconomy.io/
-    bundlerUrl: "<from biconomy dashboard>",    
-    chainId: ChainId.BASE_GOERLI_TESTNET,
+    bundlerUrl:
+      "https://bundler.biconomy.io/api/v2/5/nJPK7B3ru.dd7f7861-190d-41bd-af80-6877f74b8f44",
+    chainId: ChainId.GOERLI,
     entryPointAddress: DEFAULT_ENTRYPOINT_ADDRESS,
-  })
+  });
 
-
-  
   const paymaster: IPaymaster = new BiconomyPaymaster({
     //https://dashboard.biconomy.io/
-    paymasterUrl: '<from biconomy dashboard>' 
-  })
+    paymasterUrl:
+      "https://paymaster.biconomy.io/api/v1/5/2VPXMNQQ1.1383fcd1-47fe-4cb5-8782-3053198ed237",
+  });
 
   const connect = async () => {
     try {
-      setLoading(true)
+      setLoading(true);
       const userInfo = await particle.auth.login();
       console.log("Logged in user:", userInfo);
       const particleProvider = new ParticleProvider(particle.auth);
-      console.log({particleProvider})
+      console.log({ particleProvider });
       const web3Provider = new ethers.providers.Web3Provider(
         particleProvider,
         "any"
       );
-      setProvider(web3Provider)
+      setProvider(web3Provider);
       const biconomySmartAccountConfig: BiconomySmartAccountConfig = {
         signer: web3Provider.getSigner(),
-        chainId: ChainId.BASE_GOERLI_TESTNET,
+        chainId: ChainId.GOERLI,
         bundler: bundler,
-        paymaster: paymaster
-      }
-      let biconomySmartAccount = new BiconomySmartAccount(biconomySmartAccountConfig)
-      biconomySmartAccount =  await biconomySmartAccount.init()
-      setAddress( await biconomySmartAccount.getSmartAccountAddress())
-      setSmartAccount(biconomySmartAccount)
-      setLoading(false)
+        paymaster: paymaster,
+      };
+      let biconomySmartAccount = new BiconomySmartAccount(
+        biconomySmartAccountConfig
+      );
+      biconomySmartAccount = await biconomySmartAccount.init();
+      setAddress(await biconomySmartAccount.getSmartAccountAddress());
+      setSmartAccount(biconomySmartAccount);
+      setLoading(false);
     } catch (error) {
       console.error(error);
     }
   };
-  
+
   return (
     <>
       <Head>
@@ -85,11 +102,21 @@ export default function Home() {
       <main className={styles.main}>
         <h1>Based Account Abstraction</h1>
         <h2>Connect and Mint your AA powered NFT now</h2>
-        {!loading && !address && <button onClick={connect} className={styles.connect}>Connect to Based Web3</button>}
+        {!loading && !address && (
+          <button onClick={connect} className={styles.connect}>
+            Connect to Based Web3
+          </button>
+        )}
         {loading && <p>Loading Smart Account...</p>}
         {address && <h2>Smart Account: {address}</h2>}
-        {smartAccount && provider && <Minter smartAccount={smartAccount} address={address} provider={provider} />}
+        {smartAccount && provider && (
+          <Minter
+            smartAccount={smartAccount}
+            address={address}
+            provider={provider}
+          />
+        )}
       </main>
     </>
-  )
+  );
 }
